8cd9081a519b22ad042c7ca9a197407ab306ddaf
jasonsouryamath <jasonsouryamath@yahoo.com>
2018-10-19 15:46:56 -0700

    Update fillformpage.java

--- fillformpage.java
+++ fillformpage.java
@@ -18,8 +18,10 @@ public class fillformpage {
     By submit = By.xpath("//div[@class='et_contact_bottom_container']//button[@class='et_pb_contact_submit et_pb_button']");
     By numpar = By.xpath("//span[@class='et_pb_contact_captcha_question']");
     By captcha = By.xpath("//input[@class='input et_pb_contact_captcha']");
-    By text1 = By.xpath("//div[@class='et-pb-contact-message']//p");
+    By text1 = By.xpath("//div[@class='et-pb-contact-message']");
     //By text = By.xpath("//")
+    //By namefill = By.id("firstname");
+    By navback = By.xpath("//a[contains(text(),'Automation Exercises')]");
 
     public fillformpage(WebDriver driver){
     this.driver = driver;
@@ -48,6 +50,66 @@ public class fillformpage {
 
     public void secondbox(String strname1, String strmsg1) {
         WebElement name1 = wait.until(ExpectedConditions.elementToBeClickable(nameinput));
+        name1.sendKeys(strname1);
+
+        WebElement msg1 = wait.until(ExpectedConditions.elementToBeClickable(MsgInput));
+        msg1.sendKeys(strmsg1);
+
+        WebElement getthis = driver.findElement(numpar);
+        String thisid = getthis.getText();
+        //var + var (STRING)
+        System.out.println(thisid);
+
+        String stringsplt = thisid.trim().replaceAll("\\s+","");
+        System.out.println(stringsplt);
+
+        String[] parts = stringsplt.split(Pattern.quote("+"));
+        String a = parts[0];
+        String b = parts[1];
+        System.out.println(a + b);
+
+        int results = Integer.parseInt(a)+Integer.parseInt(b);
+        System.out.println(results);
+        String strcapo = Integer.toString(results);
+
+        WebElement capc = wait.until(ExpectedConditions.elementToBeClickable(captcha));
+        capc.sendKeys(strcapo);
+
+        WebElement submt1 = wait.until(ExpectedConditions.elementToBeClickable(submit));
+        submt1.click();
+    }
+    public void asserttext(){
+       WebElement assertthis = wait.until(ExpectedConditions.elementToBeClickable(text1));
+       String wordup = assertthis.getText();
+
+        Assert.assertEquals(myassert,wordup);
+        System.out.println(myassert);
+    }
+
+    public void webnavigatenew(){
+        WebElement newback = driver.findElement(navback);
+        newback.click();
+    }
+
+    public void fillformo(String strNameInput ,String strThisis)  {
+
+        //Fill user name
+        this.setusername(strNameInput);
+        //Fill password
+        this.setMsgInput(strThisis);
+        //Click Login button
+        this.clicksubmit();
+    }
+    public void fillsecondbox(String strname1, String strmsg1){
+        this.secondbox(strname1, strmsg1);
+    }
+    public void assertetxt(){
+        this.asserttext();
+}
+    public void sprint1() {
+         this.webnavigatenew();
+    }
+}
         name1.sendKeys(strname1);
 
         WebElement msg1 = wait.until(ExpectedConditions.elementToBeClickable(MsgInput));
