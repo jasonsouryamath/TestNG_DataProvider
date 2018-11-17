cde6cd869486d2aab45f644b88ed39cf339b1c12
jasonsouryamath <jasonsouryamath@yahoo.com>
2018-11-16 16:12:40 -0800

    Update evolvesovertime.java

--- evolvesovertime.java
+++ evolvesovertime.java
@@ -1,28 +1,45 @@
 package AutomationTest;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.WebDriverWait;
+        import org.openqa.selenium.By;
+        import org.openqa.selenium.WebDriver;
+        import org.openqa.selenium.WebElement;
+        import org.openqa.selenium.support.ui.ExpectedConditions;
+        import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class evolvesovertime {
+public class evolvesovertime  {
     WebDriver driver;
     WebDriverWait wait;
 
     By evo = By.xpath("//a[contains(text(),'Learn how to automate an application that evolves over time')]");
+    By firstname = By.xpath("//input[contains(@name,'firstname')]");
+    By lastname =  By.xpath("//input[contains(@name,'lastname')]");
+    By nxtsprt = By.xpath("//a[contains(text(),'Go to the next sprint')]");
 
     public evolvesovertime(WebDriver driver) {
         this.driver = driver;
         this.wait = new WebDriverWait(driver, 10);
     }
 
-    public void clickevolves(){
+    public void clickevolves (String first) {
         WebElement evolve = driver.findElement(evo);
         evolve.click();
+        WebElement fn = wait.until(ExpectedConditions.elementToBeClickable(firstname));
+        fn.sendKeys(first);
+        WebElement nextup = wait.until(ExpectedConditions.elementToBeClickable(nxtsprt));
+        nextup.click();
     }
 
-    public void clickingstuff (){
-        this.clickevolves();
+     public void nextmeth(String mid, String last)    {
+         WebElement fn1 = wait.until(ExpectedConditions.elementToBeClickable(firstname));
+        fn1.sendKeys(mid);
+        WebElement ln = wait.until(ExpectedConditions.elementToBeClickable(lastname));
+        ln.sendKeys(last);
     }
 
+    public void clickingstuff (String first){
+        this.clickevolves(first);
+    }
+    public void newpage(String mid, String last){
+        this.nextmeth(mid,last);
+    }
 }
