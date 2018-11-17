29798c19fb57b96da471051ee725669cb20c8905
jasonsouryamath <jasonsouryamath@yahoo.com>
2018-11-16 16:08:56 -0800

    Update testrun.java

--- testrun.java
+++ testrun.java
@@ -1,24 +1,28 @@
 package AutomationTest;
-
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import org.testng.annotations.Parameters;
+
+import java.awt.*;
+import java.io.IOException;
+import java.util.HashMap;
 
 
-public class testrun {
+public class testrun  {
     WebDriver driver;
     String myurl = "https://www.ultimateqa.com/filling-out-forms/";
     fillformpage objfillform;
     evolvesovertime objevoform;
 
     @BeforeTest
-    public WebDriver navigatetoweb() throws InterruptedException {
-        driver =new FirefoxDriver();
+    public final  WebDriver navigatetoweb() throws InterruptedException {
+        driver = new FirefoxDriver();
         //driver =new ChromeDriver();
         driver.get(myurl);
+        driver.manage().window().maximize();
 
         if ("https://www.ultimateqa.com/filling-out-forms/".equals(driver.getCurrentUrl()) == false) {
             System.err.println("Wrong URL" + driver.getCurrentUrl() + "’");
@@ -26,36 +30,47 @@ public class testrun {
         return driver;
     }
 
-    @Test (threadPoolSize = 0,priority= 1, invocationCount = 1)
-    public void testcase1_firstbox() throws InterruptedException {
+    @Test  ( threadPoolSize = 0,priority= 1, invocationCount = 1)
+    @Parameters({"name","message"})
+    public void testcase1_firstbox(String name, String message) throws InterruptedException, IOException, AWTException {
         Thread.sleep(2000);
         objfillform = new fillformpage(driver);
-        objfillform.fillformo("jason","msg");
-
+        objfillform.fillformo(name,message);
+        objfillform.screenshot();
     }
-    @Test (threadPoolSize = 0,priority= 2, invocationCount = 1)
-    public void testcase2_secondbox() throws InterruptedException {
+    @Test  ( threadPoolSize = 0,priority= 1, invocationCount = 1)
+    @Parameters({"sname","smessage"})
+    public void testcase2_secondbox(String sname,String smessage) throws InterruptedException, IOException, AWTException {
         Thread.sleep(2000);
-        objfillform.fillsecondbox("jason1","msg1");
+        objfillform.fillsecondbox(sname,smessage);
+        objfillform.screenshot();
     }
 
-    @Test (threadPoolSize = 0,priority = 3, invocationCount = 1)
-    public void testcase3_asserttext() throws InterruptedException{
+    @Test  ( threadPoolSize = 0,priority= 1, invocationCount = 1)
+    public void testcase3_asserttext() throws InterruptedException, IOException, AWTException {
         //assert the text "Form Filled out successfully" & Success after submitting both message box
         Thread.sleep(2000);
+        objfillform.screenshot();
         objfillform.assertetxt();
     }
 
-    @Test (threadPoolSize = 0, priority = 4, invocationCount= 1)
-    public void testcase4_sprint1() {
+    @Test  ( threadPoolSize = 0,priority= 1, invocationCount = 1)
+    @Parameters({"newname","newname1", "lastname"})
+    public void testcase4_sprint1(String newname, String newname1, String lastname) {
         //go through each sprint iteration and fill forms
         objfillform.webnavigatenew();
         objevoform =  new evolvesovertime (driver);
-        objevoform.clickingstuff();
-
+        objevoform.clickingstuff(newname);
+        objevoform =  new evolvesovertime (driver);
+        objevoform.newpage(newname1,lastname);
     }
     @AfterTest
     public void teardown() {
-    driver.quit();
+
+        driver.quit();
     }
+
+
+
 }
+
